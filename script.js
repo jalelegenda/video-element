5eb13b56152f6600b391dabeff1dad08af080c1b
Jakov Milić <mydayyourfuneral@yahoo.com>
2017-12-24 13:53:27 +0100

    Added showcase layout

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Enjoy our website!");
 
-
-    
     var animatedEls = {
         header: document.querySelector("header"),
         homeLogo: document.querySelector("#home-logo")
     }
+
+    
 });
 
 
