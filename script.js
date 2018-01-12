745840fe68739eb0893d408bfd5c09a3d966321f
Jakov Milić <mydayyourfuneral@yahoo.com>
2018-01-12 23:09:02 +0100

    Mobile ready, removed libraries

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Enjoy our website!");
 
-    var animatedEls = {
-        header: document.querySelector("header"),
-        homeLogo: document.querySelector("#home-logo")
+    var animEls = {
+        header:     document.querySelector("header"),
+        homeLogo:   document.querySelector("#home-logo")
     }
 
-    
-});
+    var triggers =  {
+        menuButton: document.querySelector("header button")
+    }
 
+    triggers.menuButton.addEventListener('click', function(){
+        var menuTopPadding = window.getComputedStyle(animEls.header, null)
+                                   .getPropertyValue("margin-top");
+
+        if(parseInt(menuTopPadding) < 0) {
+            animEls.header.style.marginTop = 0;
+        } else {
+            animEls.header.style.marginTop = -170;
+        }
+    });
+});
 
